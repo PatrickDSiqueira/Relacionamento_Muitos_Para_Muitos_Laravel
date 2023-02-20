f85656b7ae1d3eb8233944eb0b1e9ad424c89ae6
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-20 11:07:34 -0300

    Utilizando belongsToMany - Os projetos dos desenvolvedores

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
+use App\Projeto;
+use App\Alocacao;
+use App\Desenvolvedor;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/desenvolvedor_projetos', function (){
+    $desenvolvedores = Desenvolvedor::with('projetos')->get();
+
+    foreach ($desenvolvedores as $d){
+        echo "<p>Nome do Desenvolvedor: ".$d->nome."</p>";
+        echo "<p>Cargo: ".$d->cargo."</p>";
+        if (count($d -> projetos)>0){
+            echo "Projetos:<br>";
+            echo"<ul>";
+            foreach ($d->projetos as $p){
+                echo"<li>";
+                echo "<p>Nome do Projeto: ".$p->nome." | ";
+                echo "Horas: ".$p->estimativa_horas."</p>";
+                echo"</li>";
+            }
+            echo "</ul>";
+            }
+        echo "<hr>";
+    }
 });
